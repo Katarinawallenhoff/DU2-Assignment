85a616dc0dd348da93e9788a7de8fdc54666c7a2
Katarinawallenhoff <katarina.wallenhoff@outlook.com>
2024-11-20 12:56:18 +0100

    First loop
    This loop is just to visualize how every name in every object is displayed.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
+"use strict";
+
+const cityContainer = document.querySelector("#cities");
+
+// En for loop som bara skriver ut alla städer i en array. 
+for (let i = 0; i < cities.length; i++) {
+  const boxOfCity = document.createElement("div");
+  boxOfCity.textContent = cities[i].name;
+  boxOfCity.className = "cityBox";
+  cityContainer.appendChild(boxOfCity);
+}
+
 // Recommended: All functions declared here
+
 
 // Recommended: constants with references to existing HTML-elements
 
+
+
 // Recommended: Ask for the city name and then the rest of the code
+
+
+
+
+
+
 
